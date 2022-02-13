65d1e45d4cfd3f9e6b96aa51ca17e07803e9145c
Edwin <edwin.d899@gmail.com>
2022-02-13 12:22:50 +0100

    changement de méthode, créations des cartes mise en place des carte avec les image missing: alt (wip)

--- Kanap/front/js/script.js
+++ Kanap/front/js/script.js
@@ -1,16 +1,36 @@
-let allProducts = []
-const img = document.getElementsByTagName('img')
+    
 
 
-function fetchProducts () {
 fetch ("http://localhost:3000/api/products")
-    .then((response) =>{
-        return response.json()
+    .then(response => response.json())
+    
+    .then (data =>{
+        allProducts(data)
+        console.log(data)
     })
-    .then((allProducts) => {
-        console.log(allProducts)
-    })
-}
-console.log(allProducts.length)
-fetchProducts()
 
+function allProducts(data) {
+    for (product of data) {
+
+        // création des cartes
+
+    link = document.createElement('a');
+    article = document.createElement('article');
+    imgProduct = document.createElement('img');
+    altImg = document.createElement('alt')
+    nameProduct = document.createElement('h3');
+    descriptionProduct = document.createElement('p');
+    items = document.getElementById('items');
+    items.appendChild(link);
+    link.appendChild(article);
+    article.appendChild(imgProduct);
+    article.appendChild(nameProduct);
+    article.appendChild(descriptionProduct);
+
+    nameProduct.innerText = `${product.name}`
+    descriptionProduct.innerText = `${product.description}`
+    imgProduct.src = `${product.imageUrl}`
+    link.href = `${product._id}`
+    
+    }
+}
