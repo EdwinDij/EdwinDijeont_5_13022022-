5ab44823489e61e82c6f8e65f451d3dfef90540c
Edwin <edwin.d899@gmail.com>
2022-02-20 21:37:41 +0100

    ajout des class et des attributs

--- Kanap/front/js/script.js
+++ Kanap/front/js/script.js
@@ -1,36 +1,38 @@
-    
 
 
 fetch ("http://localhost:3000/api/products")
-    .then(response => response.json())
-    
-    .then (data =>{
-        allProducts(data)
-        console.log(data)
-    })
+.then(response => response.json())
+
+.then (data =>{
+    allProducts(data)
+    console.log(data)
+})
 
 function allProducts(data) {
-    for (product of data) {
+for (product of data) {
 
-        // création des cartes
+    // création des cartes
 
-    link = document.createElement('a');
-    article = document.createElement('article');
-    imgProduct = document.createElement('img');
-    altImg = document.createElement('alt')
-    nameProduct = document.createElement('h3');
-    descriptionProduct = document.createElement('p');
-    items = document.getElementById('items');
-    items.appendChild(link);
-    link.appendChild(article);
-    article.appendChild(imgProduct);
-    article.appendChild(nameProduct);
-    article.appendChild(descriptionProduct);
+link = document.createElement('a');
+article = document.createElement('article');
+imgProduct = document.createElement('img');
+altImg = document.createElement('alt')
+nameProduct = document.createElement('h3');
+nameProduct.classList.add("productName");
+descriptionProduct = document.createElement('p');
+descriptionProduct.classList.add ("productDescription");
+items = document.getElementById('items');
+items.appendChild(link);
+link.appendChild(article);
+article.appendChild(imgProduct);
+article.appendChild(nameProduct);
+article.appendChild(descriptionProduct);
 
-    nameProduct.innerText = `${product.name}`
-    descriptionProduct.innerText = `${product.description}`
-    imgProduct.src = `${product.imageUrl}`
-    link.href = `${product._id}`
-    
-    }
+nameProduct.innerText = `${product.name}`
+descriptionProduct.innerText = `${product.description}`
+imgProduct.src = `${product.imageUrl}`
+imgProduct.setAttribute("alt", `${product.altTxt}`)
+link.href = `${product._id}`
+
+}
 }
