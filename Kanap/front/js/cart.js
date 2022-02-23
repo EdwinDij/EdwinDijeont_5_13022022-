eef3da6c26deb2ed39ffe1cac5403678cfe5564a
Edwin <edwin.d899@gmail.com>
2022-02-23 10:36:37 +0100

    init page panier

---
+++ Kanap/front/js/cart.js
@@ -0,0 +1,6 @@
+     //creation du local storage
+
+let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+console.table(produitLocalStorage);
+const positionEmptyCart = document.querySelector("#cart__items");
+
