0e094d19213c7a12d0c699d85642967a82deb935
Edwin <edwin.d899@gmail.com>
2022-02-23 10:48:24 +0100

    debut de la boucle for in

--- Kanap/front/js/cart.js
+++ Kanap/front/js/cart.js
@@ -1,6 +1,19 @@
      //creation du local storage
 
-let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
-console.table(produitLocalStorage);
+let productLocalStorage = JSON.parse(localStorage.getItem("produit"));
+console.table(productLocalStorage);
 const positionEmptyCart = document.querySelector("#cart__items");
 
+// Si le panier est vide
+function getCart(){
+    if (productLocalStorage === null || productLocalStorage == 0) {
+        const titleCart = document.querySelector('h1')
+
+        titleCart.innerText = 'Votre panier est vide !'
+    
+    }else{
+        for (let product in productLocalStorage)
+    }
+}
+
+getCart()
